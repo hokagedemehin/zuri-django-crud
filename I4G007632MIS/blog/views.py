6abba4e1b8f161359ceae31fee0ec68b8b8514dc
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2022-06-23 14:43:49 +0100

    completed Django crud task

---
+++ I4G007632MIS/blog/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+)
+from .models import *
+from django.urls import reverse_lazy
+
+# Create your views here.
+
+
+class PostListView(ListView):
+    model = Post
+
+    fields = "__all__"
+
+    success_url = reverse_lazy("blog:all")
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
